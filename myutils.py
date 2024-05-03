1d23d080ac13ea48d230dba7fd35de5f4c3f84a2
Jihyozhixiao <77515713+Jihyozhixiao@users.noreply.github.com>
2024-05-03 20:22:40 +0900

    Final (#2)
    * final files init commit
    
    * Update function packing

--- myutils.py
+++ myutils.py
@@ -1,38 +1,39 @@
 import pandas as pd
 import numpy as np
-from numba import njit, prange
-from typing import Union, List
 
 def process_data(df):
-    # 데이터프레임 뒤집기 및 인덱스 재설정
     df = df[::-1].reset_index(drop=True)
-    
-    # 'Date' 열을 인덱스로 설정
     df.set_index('Date', inplace=True)
-    
-    # 'Change %' 열을 수치로 변환하여 'Change' 열로 추가
     df['Change'] = df['Change %'].str.rstrip('%').astype(float) / 100
-    
-    # 필요한 열들을 숫자로 변환
     numeric_cols = ['Price', 'Open', 'High', 'Low']
     for col in numeric_cols:
-        try:
-            df[col] = df[col].str.replace(',', '').astype(float)
-        except (KeyError, AttributeError):
-            pass
+        df[col] = df[col].str.replace(',', '').astype(float)
+    df.drop('Vol.', axis=1, inplace=True)
+    return df
+
+def calculate_trix(close_prices, n):
+    ema1 = close_prices.ewm(span=n, min_periods=n).mean()
+    ema2 = ema1.ewm(span=n, min_periods=n).mean()
+    ema3 = ema2.ewm(span=n, min_periods=n).mean()
     
-    # 'Vol.' 열 삭제
-    try:
-        df.drop('Vol.', axis=1, inplace=True)
-    except KeyError:
-        pass
-    
+    trix = (ema3 - ema3.shift(1)) / ema3.shift(1) * 100
+    return trix
+
+def set_signal(df):
+    df['trix'] = calculate_trix(df['Price'], n=5)
+    df['buy_signal'] = False
+    df['sell_signal'] = False
+    df['trix'] = df['trix'].fillna(0)
+
+    for i in range(1, len(df)):
+        if df.iloc[i]['trix'] < 0 and df.iloc[i-1]['trix'] > 0:
+            df.at[df.index[i], 'buy_signal'] = True
+        elif df.iloc[i]['trix'] > 0 and df.iloc[i-1]['trix'] < 0:
+            df.at[df.index[i], 'sell_signal'] = True
     return df
 
 def get_historical_var(df, window_size, percentile):
     df['VaR'] = df['Change'].rolling(window=window_size).quantile(percentile, interpolation='lower')
-    # df.dropna(subset=['VaR'], inplace=True) 
-        # NaN 드랍하기
     return df
 
 def calculate_daily_rf(df, days):
@@ -40,29 +41,7 @@ def calculate_daily_rf(df, days):
     df = df.set_index('Date')
     return df
 
-# @njit(cache=True)
-# def get_rolling_kelly(
-#     df, 
-#     # returns: Union[pd.DataFrame, pd.Series],
-#     window: int = 400,
-#     r: float = 0.02,
-#     days: int = 250,
-# ) -> pd.DataFrame:
-# #     r = (1 + r) ** (1 / days) - 1
-#     returns = df['Change']
-#     mean = returns.rolling(window).mean().dropna()
-#     print(mean)
-#     return_exces = mean - r
-
-#     var = returns.rolling(window).var().dropna()
-#     kelly = return_exces / var
-
-#     return kelly
-
-def get_rolling_kelly(
-    df, 
-    window: int = 400,
-) -> pd.DataFrame:
+def get_rolling_kelly(df, window: int = 400):
     mean = df['Change'].rolling(window).mean().dropna()
     return_exces = mean -  df['rf']
 
@@ -72,31 +51,17 @@ def get_rolling_kelly(
 
     return kelly
 
-def get_cumulative_returns(df, rebalancing):
-    portfolio = np.zeros(len(df))
-    equity = np.zeros(len(df))
-    cash = np.zeros(len(df))
-
-    for i, _row in enumerate(df.iterrows()):
-        row = _row[1]
-        if i == 0:
-            portfolio[0] = 1
-            cash[0] = 1-row['kelly_ratio']
-            equity[0] = row['kelly_ratio']
-        else:
-            portfolio[i] = cash[i-1] + equity[i-1]
-            # portfolio[i] = equity[i-1] * (1 + row['Change']) + cash[i-1] * (1 + row['rf'])
-            if i % rebalancing == 0:
-                equity[i] = portfolio[i] * row['kelly_ratio']
-                cash[i] = portfolio[i] * (1 - row['kelly_ratio'])
-            else:
-                equity[i] = equity[i-1] * (1 + row['Change'])
-                cash[i] = cash[i-1] * (1 + row['rf'])
-    df.loc[:, 'portfolio'] = portfolio
-    df.loc[:, 'equity'] = equity
-    df.loc[:, 'cash'] = cash
-
-    return df
+def calculate_capped_kelly(kelly_criterion):
+    if kelly_criterion > 5:
+        return 5
+    else:
+        return 0 if kelly_criterion < 0 else kelly_criterion
+    
+def update_var_kelly(row):
+    if row['VaR'] > row['Change']:
+        return 0
+    else:
+        return row['kelly_ratio']
 
 def get_cumulative_trix_returns(df):
     portfolio = np.zeros(len(df))
@@ -112,17 +77,13 @@ def get_cumulative_trix_returns(df):
         else:
             portfolio[i] = equity[i-1] * (1 + row['Change']) + cash[i-1] * (1 + row['rf'])
             if row['buy_signal']:
-                portfolio[i] = equity[i-1] * (1 + row['Change']) + cash[i-1] * (1 + row['rf'])
                 if portfolio[i] < 0:
-                    equity[i] = equity[i-1] * (1 + row['Change']) + row['kelly_ratio']
-                    cash[i] = cash[i-1] * (1 + row['rf']) - row['kelly_ratio']
-                    # equity[i] = equity[i-1] * (1 + row['Change'])
-                    # cash[i] = cash[i-1] * (1 + row['rf'])
+                    equity[i] = equity[i-1] * (1 + row['Change']) 
+                    cash[i] = cash[i-1] * (1 + row['rf']) 
                 else:
                     equity[i] = portfolio[i] * row['kelly_ratio'] 
                     cash[i] = portfolio[i] * (1 - row['kelly_ratio'])                   
             elif row['sell_signal']:
-                portfolio[i] = equity[i-1] * (1 + row['Change']) + cash[i-1] * (1 + row['rf'])
                 equity[i] = 0
                 cash[i] = portfolio[i]
             else:
